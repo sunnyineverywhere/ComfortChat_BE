06d5fa4c5bce73cdc1c9d505a4f9136a90885909
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-01-12 15:59:49 +0900

    feat: create project

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.redirect_slashes = False
+
+
+@app.get("/hello")
+async def root():
+    return {"message": "Hello! We are ComfortChat!"}
